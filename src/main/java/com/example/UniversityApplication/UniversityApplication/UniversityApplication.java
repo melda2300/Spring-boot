2b18d468ed57e224e280a5b378cb843d939b8cd4
melda2300 <146105556+melda2300@users.noreply.github.com>
2024-05-13 14:07:43 +0300

    yenilendi

--- src/main/java/com/example/UniversityApplication/UniversityApplication/UniversityApplication.java
+++ src/main/java/com/example/UniversityApplication/UniversityApplication/UniversityApplication.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.example.UniversityApplication.UniversityApplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UniversityApplication  {
+public class UniversityApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UniversityApplication.class, args);
