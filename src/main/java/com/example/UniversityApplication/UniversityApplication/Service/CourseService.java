e211e8072425ea7da213ac650fa7a5cab015bf8f
melda2300 <146105556+melda2300@users.noreply.github.com>
2024-05-14 01:20:05 +0300

    sercisler yüklendi

--- src/main/java/com/example/UniversityApplication/UniversityApplication/Service/CourseService.java
+++ src/main/java/com/example/UniversityApplication/UniversityApplication/Service/CourseService.java
@@ -1,4 +1,26 @@
 package com.example.UniversityApplication.UniversityApplication.Service;
 
+import com.example.UniversityApplication.UniversityApplication.Class.Course;
+import com.example.UniversityApplication.UniversityApplication.Repository.CourseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class CourseService {
+    @Autowired
+    private CourseRepository courseRepository;
+
+    public List<Course> getAllCourses() {
+        return courseRepository.findAll();
+    }
+
+    public Course getCourseById(Long id) {
+        return courseRepository.findById(id).orElseThrow();
+    }
+
+    public Course createCourse(Course course) {
+        return courseRepository.save(course);
+    }
 }
--- src/main/java/com/example/UniversityApplication/UniversityApplication/Service/StudentService.java
+++ src/main/java/com/example/UniversityApplication/UniversityApplication/Service/StudentService.java
@@ -1,4 +1,26 @@
 package com.example.UniversityApplication.UniversityApplication.Service;
 
+import com.example.UniversityApplication.UniversityApplication.Class.Student;
+import com.example.UniversityApplication.UniversityApplication.Repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class StudentService {
+    @Autowired
+    private StudentRepository studentRepository;
+
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
+
+    public Student getStudentById( Long id ) {
+        return studentRepository.findById(id).orElseThrow();
+    }
+
+    public Student createStudent( Student student ) {
+        return studentRepository.save(student);
+    }
 }
