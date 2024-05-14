6b307ee17e6193e5ac65bf7fd125e3527461125c
melda2300 <146105556+melda2300@users.noreply.github.com>
2024-05-14 11:47:40 +0300

    controller yüklendi

--- src/main/java/com/example/UniversityApplication/UniversityApplication/Controller/CourseController.java
+++ src/main/java/com/example/UniversityApplication/UniversityApplication/Controller/CourseController.java
@@ -1,9 +1,28 @@
 package com.example.UniversityApplication.UniversityApplication.Controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.UniversityApplication.UniversityApplication.Class.Course;
+import com.example.UniversityApplication.UniversityApplication.Service.CourseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(name = "/Courses")
 public class CourseController {
+    @Autowired
+    private CourseService courseService;
+
+    @GetMapping
+    public List<Course> getAllCourses(){
+        return courseService.getAllCourses();
+    }
+    @GetMapping(name = "/{id}")
+    public Course getCoursesById( @PathVariable Long id){
+        return courseService.getCourseById(id);
+    }
+    @PostMapping
+    public Course createCourse(@RequestBody Course course){
+        return  courseService.createCourse(course);
+    }
 }
