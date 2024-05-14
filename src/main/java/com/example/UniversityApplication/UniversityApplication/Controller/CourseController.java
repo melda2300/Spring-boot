81959cadf84a16696df2f2d98a5e21366f51aed4
melda2300 <146105556+melda2300@users.noreply.github.com>
2024-05-14 11:28:11 +0300

    controller yüklendi

--- src/main/java/com/example/UniversityApplication/UniversityApplication/Controller/CourseController.java
+++ src/main/java/com/example/UniversityApplication/UniversityApplication/Controller/CourseController.java
@@ -1,4 +1,9 @@
 package com.example.UniversityApplication.UniversityApplication.Controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(name = "/Courses")
 public class CourseController {
 }
--- src/main/java/com/example/UniversityApplication/UniversityApplication/Controller/StudentController.java
+++ src/main/java/com/example/UniversityApplication/UniversityApplication/Controller/StudentController.java
@@ -1,4 +1,14 @@
 package com.example.UniversityApplication.UniversityApplication.Controller;
 
+import com.example.UniversityApplication.UniversityApplication.Service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(name = "/sutudent")
 public class StudentController {
+    @Autowired
+    private StudentService studentService;
+    
 }
